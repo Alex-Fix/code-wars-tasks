eaedcda475dc74064347adf397a318bc5439982f
Alex Fix <oleksandr.papish02052001@gmail.com>
2021-08-31 22:19:18 +0300

    Josefus Suervivol

---
+++ JavaScript/src/josefusSurvivol.js
@@ -0,0 +1,44 @@
+/*
+
+In this kata you have to correctly return who is the "survivor", ie: the last element of a Josephus permutation.
+
+Basically you have to assume that n people are put into a circle and that they are eliminated in steps of k elements, like this:
+
+josephus_survivor(7,3) => means 7 people in a circle;
+one every 3 is eliminated until one remains
+[1,2,3,4,5,6,7] - initial sequence
+[1,2,4,5,6,7] => 3 is counted out
+[1,2,4,5,7] => 6 is counted out
+[1,4,5,7] => 2 is counted out
+[1,4,5] => 7 is counted out
+[1,4] => 5 is counted out
+[4] => 1 counted out, 4 is the last element - the survivor!
+The above link about the "base" kata description will give you a more thorough insight about the origin of this kind of permutation, but basically that's all that there is to know to solve this kata.
+
+Notes and tips: using the solution to the other kata to check your function may be helpful, but as much larger numbers will be used, using an array/list to compute the number of the survivor may be too slow; you may assume that both n and k will always be >=1.
+
+*/
+
+function josefusSurvivol(n, k) {
+    const array = [];
+    for(let i = 1; i <= n; ++i) 
+        array.push(i);
+
+    let index = 0, counter = 1;
+    while(true) {
+        if(array.length == 1) 
+            return array.shift();
+
+        if(array.length == index) 
+            index = 0;
+
+        if(counter % k == 0) 
+            array.splice(index, 1);
+        else 
+            ++index;
+
+        ++counter;
+    }
+}
+
+console.log(josefusSurvivol(7, 3));
